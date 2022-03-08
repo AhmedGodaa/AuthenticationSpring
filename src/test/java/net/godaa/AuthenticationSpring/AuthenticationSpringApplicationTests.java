cce6718e1a7428b4bfd9b605af0f106c0973f661
ahmedgodaa <ana.a7med1140@gmail.com>
2022-03-08 02:09:42 +0200

    Initial commit

---
+++ src/test/java/net/godaa/AuthenticationSpring/AuthenticationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package net.godaa.AuthenticationSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
